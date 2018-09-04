81c2b9d72ab5cc49b0160ba9e2e09587001f4276
GuillaumeMarcel <42968106+GuillaumeMarcel@users.noreply.github.com>
2018-09-04 15:09:46 +0200

    Add files via upload

---
+++ Easy_Quizz/pages/Accueil.php
@@ -0,0 +1,8 @@
+<?php
+  include("Includes/Entete.php");
+?>
+<div class="Block1">
+  <h2>EASY Quizz</h2>
+  <p class="p1">Easy Quizz est un Quizz à été créé par des éleves de Easy School, lors d'un projet de cours
+  pour permettre aux professeurs de tester les connaissances de ses élèves en ligne.</p>
+</div>
---
+++ Easy_Quizz/pages/Entete.php
@@ -0,0 +1,28 @@
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>EASY Quizz</title>
+    <link rel="icon" type="icon" href="../images/ico.ico" />
+    <link href="../Style/Style1.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<h1>EASY Quizz</h1>
+<div class="DivLink1">
+    <div class="DivLink2">
+      <a href="../Accueil.php"><p class="Link">ACCUEIL</p></a>
+    </div>
+    <div class="DivLink2" style="width:15%;">
+      <a href="../Connexion.php"><p class="Link">CONNEXION</p></a>
+    </div>
+    <div class="DivLink2">
+        <a href="../Quizz.php"><p class="Link">LE QUIZZ</p></a>
+    </div>
+    <div class="DivLink2">
+        <a href="../Matieres.php"><p class="Link">MATIERES</p></a>
+    </div>
+    <div class="DivLink2">
+      <a href="../Contact.php"><p class="Link">CONTACT</p></a>
+    </div>
+</div>
+<?php
+?>
